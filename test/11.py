c96a71f043bb202a7fada73dd874f7d816b360da
YY zhang <78712778+YY-Zhang1999@users.noreply.github.com>
2025-05-09 14:27:58 -0500

    5.9

--- test/11.py
+++ test/11.py
@@ -2,7 +2,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 a = pd.read_pickle('clustering_Fu_1_sim_speed_500_data.csv')
+a = a[:-4]
 
-
-plt.plot(a[10])
+plt.plot(a.iloc[1])
 plt.show()
